97c93e18d770e83d6309b08a7da700dbbd291e87
Ubuntu <ubuntu@vps181728.vps.ovh.ca>
2018-09-27 19:13:53 -0400

    begin writing asset balance engine

---
+++ asset_engine/config.py
@@ -0,0 +1,25 @@
+mq_host = 'localhost'
+mq_port = 1883
+mq_keepalive = 60
+mq_bindAddress = ""
+mq_user = 'vibot'
+mq_pass = 'NmQ5Nj_3MrAwiNDu'
+mq_subtop = 'trade'
+mq_pubtop = 'messages'
+
+poloniexKey = '3522RD7Z-8BPPZOS8-7WWA9XRN-OAAVNSIG'
+poloniexSecret = '04efd04abdbf672cfb1dea830affe489a7c347fe46aa6b79f4f85f33e2f1411c2858c199633678a4e727e03c9ecabc5fc28f37d4e8fb82540660bfe68593ce2b'
+
+cexUser = 'up109970818'
+cexKey = 'GKVYOLKQnmMIY5Qn9026cVK2I1A'
+cexSecret = '2Aw2B2O8RFx93ifAfJFnVS51jaY'
+
+bittrexKey = 'f18ac03ce7da4bd7a02e766b9b2912c5'
+bittrexSecret = 'd3374fed38b14e988f7905400e5d28b2'
+
+binanceKey = 'vURW3wM0yC7vD4RwAZIEFbMsDND5SIXIj0lGWhcv4RlNVlXtk0EVV3bSq9xrC8Jh'
+binanceSecret = 'r0XkzQBMC6xHXTz7IE9DF9yHmwqWBsuul3lJlX6TfT8YCqqYViItBSdtEkBdZ539'
+
+okexKey = '77f7bb10-6ae4-44c6-8eeb-9710a5fa86d8'
+okexSecret = '3E0BA281918F51897158E1B1D10B6D70'
+
