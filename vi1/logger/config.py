0cc6591aa9fb6a2633d81099782db8eeb12d8849
Ubuntu <ubuntu@vps181728.vps.ovh.ca>
2018-09-27 20:06:48 -0400

    add new balance calc

---
+++ vi1/logger/config.py
@@ -0,0 +1,21 @@
+mq_host = 'localhost'
+mq_port = 1883
+mq_keepalive = 60
+mq_bindAddress = ""
+mq_user = 'vibot'
+mq_pass = 'NmQ5Nj_3MrAwiNDu'
+mq_subtop = 'trade'
+mq_subtop_pbal = 'pbal'
+mq_subtop_cbal = 'cbal'
+mq_pubtop = 'messages'
+
+poloniexKey = 'ITZGVATK-DEVEHW5N-Z5VP6G2E-L5NRML6V'
+poloniexSecret = '1a4e05a3a8b7642534bb700b929d4b784f80e5ba17c87bc72376fd75ccb663dcfaca967769f3e8a9376acafe9f14fb147bf593cc13434e0dbf6e67de6f9f4a28'
+
+cexUser = 'up109970818'
+cexKey = 'GKVYOLKQnmMIY5Qn9026cVK2I1A'
+cexSecret = '2Aw2B2O8RFx93ifAfJFnVS51jaY'
+
+bittrexKey = 'f18ac03ce7da4bd7a02e766b9b2912c5'
+bittrexSecret = 'd3374fed38b14e988f7905400e5d28b2'
+
